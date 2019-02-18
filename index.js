9d8277cbd721f8a5738f08a320b71d1afe569f97
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-18 14:15:48 -0500

    setup update, then and catch on put method.

--- index.js
+++ index.js
@@ -52,6 +52,27 @@ server.delete('/hubs/:id', (req, res) => {
     });
 });
 
+server.put('/hubs/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db.hubs
+    .update(id, changes)
+    .then(updated => {
+        if(updated) {
+            res.status(200).json({ success: true, updated });
+        } else {
+            res.status(404).json({ 
+                sucess: false, 
+                message: 'I cannot find the hub you are looking for',
+            });
+        }
+    })
+    .catch(({ code, message }) => {
+        res.status(code).json({ success: false, message });
+    });
+});
+
 server.listen(4000, () => {
     console.log(`\n *** Running on port 4000 ***\n`)
 });
